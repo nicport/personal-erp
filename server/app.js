80501bc778087b0db35052bd9742ca809f4acf33
nicport <nicporter1013@gmail.com>
2023-09-06 22:46:31 -0400

    some minor changes, trying to get a working version that performs CRUD operations for transactions

--- server/app.js
+++ server/app.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const transactionsRoutes = require('./routes/transactions');
+const tasksRoutes = require('./routes/tasks');
+const db = require('./db/index.js');
+const port = 5000;
 
 app.use(cors());
 app.use(express.json());
 
+// Create transactions table if not exists
+db.run(`CREATE TABLE IF NOT EXISTS transactions (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  description TEXT,
+  amount REAL,
+  type TEXT
+);`);
+
+/*
 app.get("/api/transactions", (req, res) => {
   // Normally this data would come from the database
   const transactions = [
@@ -14,8 +27,10 @@ app.get("/api/transactions", (req, res) => {
 
   res.json(transactions);
 });
+*/
 
-const port = 5000;
+app.use('/api/transactions', transactionsRoutes);
+app.use('/api/tasks', tasksRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
