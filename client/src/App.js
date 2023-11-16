3241e08f035b3176c67b412424c8bbc5340e31c5
nicport <nicporter1013@gmail.com>
2023-11-16 18:44:26 -0500

    updates to the styling and format of the application - still needs to be updated but this is a good starting point

--- client/src/App.js
+++ client/src/App.js
@@ -9,7 +9,7 @@ const App = () => {
   const [error, setError] = useState(null); // add an error state
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
-  const transactionTypes = ["Income", "Expense", "Transfer", "Other"];
+  const transactionTypes = ["Income", "Travel", "Food", "Entertainment", "Health & Fitness"];
 
   const columns = React.useMemo(
     () => [
@@ -124,20 +124,33 @@ const App = () => {
 
   return (
     <div>
-      <CSVUpload onFileLoaded={handleCSVData} />
-      <input
-        type="date"
-        value={startDate}
-        onChange={(e) => setStartDate(e.target.value)}
-      />
-      <input
-        type="date"
-        value={endDate}
-        onChange={(e) => setEndDate(e.target.value)}
-      />
-      <div className='table'>
-        {error && <p>Error: {error}</p>} {/* display error message to user */}
-        <TransactionsTable data={filteredTransactions} columns={columns} />
+      <div className='toolbar'>
+        <div className='csv-upload'>
+          <CSVUpload onFileLoaded={handleCSVData} />
+        </div>
+        <div className='date-filter'>
+          <h6>Date Range Filter:</h6>
+          <div>
+            <input
+              type="date"
+              value={startDate}
+              onChange={(e) => setStartDate(e.target.value)}
+            />
+          </div>
+          <div>
+            <input
+              type="date"
+              value={endDate}
+              onChange={(e) => setEndDate(e.target.value)}
+            />
+          </div>
+        </div>
+      </div>
+      <div className='display-data'>
+        <div className='table'>
+          {error && <p>Error: {error}</p>} {/* display error message to user */}
+          <TransactionsTable data={filteredTransactions} columns={columns} />
+        </div>
       </div>
     </div>
   );
