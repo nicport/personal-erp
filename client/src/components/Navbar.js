ceec751fb113a64af6ca8273ce6f27c2fc4076b4
nicport <nicporter1013@gmail.com>
2023-11-26 22:55:29 -0500

    small change to navbar formatting

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Navbar() {
   return (
     <nav className='navbar'>
-      <h1>PersonalResourcePlanning</h1>
+      <h4>PersonalResourcePlanning</h4>
       <ul className='nav-links'>
         <li><a href="/">Home</a></li>
         <li><a href='about'>About</a></li>
