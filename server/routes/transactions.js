47e86734f335f5baa38cfe9b6652e67b2aa37db5
nicport <nicporter1013@gmail.com>
2023-11-27 22:18:29 -0500

    refactoring code to use async await instead of callbacks

--- server/routes/transactions.js
+++ server/routes/transactions.js
@@ -1,45 +1,47 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../db/index.js');
+const { getAllTransactions, getTransaction, postTransaction, updateTransaction, deleteTransaction } = require('../db/dbUtils');
 
 // GET all transactions
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   const sql = 'SELECT * FROM transactions';
-  db.all(sql, [], (err, rows) => {
-    if (err) {
-      throw err;
-    }
+  try {
+    const rows = await getAllTransactions(sql, []);
     res.json(rows);
-  });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 // GET a specific transaction by id
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   const sql = `SELECT * FROM transactions WHERE id = ?`;
-  db.get(sql, [req.params.id], (err, row) => {
-    if (err) {
-      throw err;
-    }
+  try {
+    const row = await getTransaction(sql, [req.params.id]);
     res.json(row);
-  });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 // POST a new transaction
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const { date, description, amount, type } = req.body;
   const sql = `INSERT INTO transactions (date, description, amount, type) VALUES (?, ?, ?, ?)`;
-  db.run(sql, [date, description, amount, type], function (err) {
-    if (err) {
-      throw err;
-    }
-    res.json({ id: this.lastID, message: "Transaction inserted successfully" });
-  });
+
+  try {
+    res.json(await postTransaction(sql, [date, description, amount, type]));
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 // POST bulk transactions
 router.post('/bulk', (req, res) => {
   const transactions = req.body.transactions;
-  const sql = "INSERT INTO transactions (date, description, amount, type) VALUES (?, ?, ?, ?)";
+  const sqlCheckDuplicate = `SELECT * FROM transactions WHERE date = ? AND description = ? AND amount = ?`;
+  const sqlInsert = "INSERT INTO transactions (date, description, amount, type) VALUES (?, ?, ?, ?)";
 
   db.serialize(() => {
     db.run("BEGIN TRANSACTION");
@@ -57,26 +59,26 @@ router.post('/bulk', (req, res) => {
 });
 
 // PUT (update) a transaction
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   const { description, amount, type } = req.body;
   const sql = `UPDATE transactions SET description = ?, amount = ?, type = ? WHERE id = ?`;
-  db.run(sql, [description, amount, type, req.params.id], function (err) {
-    if (err) {
-      throw err;
-    }
-    res.json({ changes: this.changes });
-  });
+
+  try {
+     res.json(await updateTransaction(sql, [description, amount, type]));
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 // DELETE a transaction
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   const sql = `DELETE FROM transactions WHERE id = ?`;
-  db.run(sql, [req.params.id], function (err) {
-    if (err) {
-      throw err;
-    }
-    res.json({ changes: this.changes });
-  });
+
+  try {
+    res.json(await deleteTransaction(sql, [req.params.id]));
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 });
 
 module.exports = router;
