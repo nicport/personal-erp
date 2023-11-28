f082fc39849a82f521b8416e2498f138f6080120
nicport <nicporter1013@gmail.com>
2023-11-27 22:17:33 -0500

    added dbUtils to separate db logic from route logic

---
+++ server/db/dbUtils.js
@@ -0,0 +1,70 @@
+// dbUtils.js
+const db = require('./index.js');
+
+const getAllTransactions = (sql, params) => {
+  return new Promise((resolve, reject) => {
+    db.all(sql, params, (err, rows) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(rows);
+      }
+    });
+  });
+};
+
+const getTransaction = (sql, params) => {
+  return new Promise((resolve, reject) => {
+    db.get(sql, [params], (err, row) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(row);
+      }
+    });
+  })
+}
+
+const postTransaction = (sql, params) => {
+  return new Promise((resolve, reject) => {
+    db.run(sql, params, function (err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({ id: this.lastID, message: "Transaction inserted successfully" });
+      }
+    });
+  })
+}
+
+const updateTransaction = (sql, params) => {
+  return new Promise((resolve, reject) => {
+    db.run(sql, [params], function (err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({ changes: this.changes });
+      }
+    });
+  })
+}
+
+const deleteTransaction = (sql, params) => {
+  return new Promise((resolve, reject) => {
+    db.run(sql, [params], function (err) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve({ changes: this.changes });
+      }
+    });
+  })
+}
+
+module.exports = {
+  getAllTransactions,
+  getTransaction,
+  postTransaction,
+  updateTransaction,
+  deleteTransaction
+};
