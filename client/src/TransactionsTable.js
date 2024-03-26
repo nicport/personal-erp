f9c3807ab7c76e8b0094751b20c871d1ca3393cb
nicport <nicporter1013@gmail.com>
2024-03-26 16:57:59 -0400

    working on adding infinite scroll

--- client/src/TransactionsTable.js
+++ client/src/TransactionsTable.js
@@ -1,40 +1,51 @@
 import React, { useEffect } from 'react';
 import { useTable } from 'react-table';
+import { FixedSizeList as List } from 'react-window';
 
 function TransactionsTable({ data, columns }) {
-  const {
+    const {
       getTableProps,
       getTableBodyProps,
       headerGroups,
       rows,
       prepareRow,
-  } = useTable({ columns, data });
-
-  return (
+    } = useTable({ columns, data });
+  
+    const RenderRow = ({ index, style }) => {
+      const row = rows[index];
+      prepareRow(row);
+      return (
+        <tr {...row.getRowProps({ style })}>
+          {row.cells.map(cell => {
+            return <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
+          })}
+        </tr>
+      );
+    };
+  
+    return (
       <table {...getTableProps()}>
-          <thead>
-              {headerGroups.map(headerGroup => (
-                  <tr {...headerGroup.getHeaderGroupProps()}>
-                      {headerGroup.headers.map(column => (
-                          <th {...column.getHeaderProps()}>{column.render('Header')}</th>
-                      ))}
-                  </tr>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>{column.render('Header')}</th>
               ))}
-          </thead>
-          <tbody {...getTableBodyProps()}>
-              {rows.map(row => {
-                  prepareRow(row);
-                  return (
-                      <tr {...row.getRowProps()}>
-                          {row.cells.map(cell => (
-                              <td {...cell.getCellProps()}>{cell.render('Cell')}</td>
-                          ))}
-                      </tr>
-                  );
-              })}
-          </tbody>
+            </tr>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          <List
+            height={400} // Adjust as needed
+            itemCount={rows.length}
+            itemSize={35} // Adjust based on the height of your rows
+            width='100%'
+          >
+            {RenderRow}
+          </List>
+        </tbody>
       </table>
-  );
-}
+    );
+  }
 
 export default TransactionsTable;
